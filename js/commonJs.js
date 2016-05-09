112d701407cb13e60ebb02ff975fed28eff77767
Nimmerz <funeralfear@gmail.com>
2016-05-09 17:30:24 +0300

    adding links & nav product menu

--- js/commonJs.js
+++ js/commonJs.js
@@ -52,4 +52,13 @@ $(document).ready(function() {
     $(window).load(function() {
         $('.wrap-loader').css('display', 'none');
     });
+
+    $('nav li ul').hide().removeClass('fallback');
+    $('nav li').hover(
+    function () {
+       $('ul', this).stop().slideDown(400);
+     },
+    function () {
+    $('ul', this).stop().slideUp(400);
+      });
 });
